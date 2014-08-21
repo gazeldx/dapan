d0744197677357d618dfcb938b610827126bb40d
Lane <zjloveztt@gmail.com>
2014-08-21 17:16:30 +0800

    5

--- app/models/index.rb
+++ app/models/index.rb
@@ -1,7 +1,7 @@
 class Index < ActiveRecord::Base
   
   include Trading
-  
+  extend Trading
   validates :current_date, uniqueness: true#TODO should not be holiday and sunday.
   validates_numericality_of :closing_price, :opening_price
   validates_numericality_of :advance, only_integer: true, allow_nil: true
@@ -13,6 +13,10 @@ class Index < ActiveRecord::Base
   def self.auto_generate
     puts "hello baby auto Time is #{Time.now} "
     Rails.logger.warn "auto_generate and Time is #{Time.now} "
+    create_an_index if trading_day?(Date.today)
+  end
+  
+  def self.create_an_index
     source = Net::HTTP.get('hq.sinajs.cn', "/list=sh000001")
     m = source.split(',')
     puts "source is #{source}"
@@ -24,7 +28,7 @@ class Index < ActiveRecord::Base
   def set_upshot
     yesterday_closing_price = Index.find_by_current_date(previous_trading_day(self.current_date)).try(:closing_price)
     if yesterday_closing_price.nil?
-      logger.error "#{self.current_date} 's previous trading day's closing price is nil!"
+      logger.error "Warn: #{self.current_date} 's previous trading day's closing price is nil!"
     else
       range = (closing_price - yesterday_closing_price) / yesterday_closing_price
       if range > FLAT_MAX
