b163506abeff3af7767fa7618c12a3cc44877673
Lane <zjloveztt@gmail.com>
2014-09-08 21:02:46 +0800

    holiday

--- config/environment.rb
+++ config/environment.rb
@@ -12,5 +12,5 @@ FLAT_MAX = 0.0035
 ADF = ['', I18n.t('decline'), I18n.t('advance'), I18n.t('flat')]
 AD = {advance: I18n.t('_advance'), decline: I18n.t('_decline')}
 DEFAULT_NICKNAME = I18n.t('default_nickname')
-HOLIDAYS = ['2014-08-05', '2014-08-06', '2014-08-07', '2014-08-08', '2014-08-09', '2014-09-06']
+HOLIDAYS = ['2014-09-08', '2014-10-01', '2014-10-02', '2014-10-03', '2014-10-06', '2014-10-07']
 COLOR = ['', 'success', 'danger', 'warning']
