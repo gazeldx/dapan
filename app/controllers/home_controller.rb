17d30975208d31f80dd6bf5724713c8ba5448fdf
Lane <zjloveztt@gmail.com>
2014-09-18 22:00:46 +0800

    about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
   def index
     @votes = Vote.includes(:user).where(target_date: get_target_date_all_ok)
     @advance_count, @decline_count, @flat_count = 0, 0, 0
-    @votes.where(upshot: ADVANCE).each { |vote| @advance_count += vote.user.all_kill > 0 ? vote.user.all_kill : 1 }
-    @votes.where(upshot: DECLINE).each { |vote| @decline_count += vote.user.all_kill > 0 ? vote.user.all_kill : 1 }
-    @votes.where(upshot: FLAT).each { |vote| @flat_count += vote.user.all_kill > 0 ? vote.user.all_kill : 1 }
+    @votes.where(upshot: ADVANCE).each { |vote| @advance_count += vote.user.all_kill > 0 ? vote.user.all_kill + 1 : 1 }
+    @votes.where(upshot: DECLINE).each { |vote| @decline_count += vote.user.all_kill > 0 ? vote.user.all_kill + 1 : 1 }
+    @votes.where(upshot: FLAT).each { |vote| @flat_count += vote.user.all_kill > 0 ? vote.user.all_kill + 1 : 1 }
     
     @aces = User.where('all_kill > 0').order(all_kill: :desc).limit(10)
 
